1115780fbb76d7a6587373272727321806c222de
Vipul <vipulnayak1192@gmail.com>
2023-08-09 12:10:02 +0530

    fixed some errors

--- src/components/NewsItems.js
+++ src/components/NewsItems.js
@@ -1,13 +1,19 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export class NewsItems extends Component {
-    render() {
-        let { title, description, imageUrl, newsUrl, author, date, source } = this.props;
+const NewsItem = (props)=> {
+        let { title, description, imageUrl, newsUrl, author, date, source } = props;
         return (
             <div className="my-3">
                 <div className="card">
-                <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left: '90%', zIndex: '1'}}> {source}
-                        </span>
+                    <div style={{
+                        display: 'flex',
+                        justifyContent: 'flex-end',
+                        position: 'absolute',
+                        right: '0'
+                    }
+                    }> 
+                        <span className="badge rounded-pill bg-danger"> {source} </span>
+                    </div>
                     <img src={!imageUrl ? "https://fdn.gsmarena.com/imgroot/news/21/08/xiaomi-smart-home-india-annoucnements/-476x249w4/gsmarena_00.jpg" : imageUrl} className="card-img-top" alt="..." />
                     <div className="card-body">
                         <h5 className="card-title">{title}  </h5>
@@ -18,7 +24,7 @@ export class NewsItems extends Component {
                 </div>
             </div>
         )
-    }
+     
 }
 
-export default NewsItems
+export default NewsItem
